c85b7ea181e58d2875eb0d86d3a19c22309f5c9d
Rhythm1821 <106919276+Rhythm1821@users.noreply.github.com>
2023-02-16 12:33:18 +0530

    Add files via upload

---
+++ FlaskBlog/flaskblog/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+    SECRET_KEY = '9c492c5f6e48c4680987b6f9fd8c5685'
+    SQLALCHEMY_DATABASE_URI= 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'alamrawat1821@gmail.com'
+    MAIL_PASSWORD = "vjebholcknscktbt"
